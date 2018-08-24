1c8b00962b58bde9daed3d9b58b69f6d81aa69b2
youxun <13261656101@163.com>
2018-08-24 23:54:25 +0800

    yml文件修改

--- fmkj-order/order-server/src/main/java/com/fmkj/order/server/controller/HcAccountController.java
+++ fmkj-order/order-server/src/main/java/com/fmkj/order/server/controller/HcAccountController.java
@@ -1,9 +1,15 @@
 package com.fmkj.order.server.controller;
 
+import com.baidu.unbiz.fluentvalidator.ComplexResult;
+import com.baidu.unbiz.fluentvalidator.FluentValidator;
+import com.baidu.unbiz.fluentvalidator.ResultCollectors;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.fmkj.common.base.BaseApiService;
 import com.fmkj.common.base.BaseController;
 import com.fmkj.common.base.BaseResult;
+import com.fmkj.common.base.BaseResultEnum;
+import com.fmkj.common.validator.LengthValidator;
+import com.fmkj.common.validator.NotNullValidator;
 import com.fmkj.order.dao.domain.HcAccount;
 import com.fmkj.order.server.service.HcAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +34,29 @@ public class HcAccountController extends BaseController<HcAccount, HcAccountServ
 
     @GetMapping(value = "selectPage")
     public BaseResult<Page<HcAccount>> selectPage(@RequestParam Map<String, Object> params) {
+        Query<HcAccount> query = new Query<HcAccount>(params);
+        // 参数校验
+        ComplexResult validatResult = FluentValidator.checkAll()
+                .on((String) params.get("telephone"), new LengthValidator(1, 13, "电话号码"))
+                .on((String) params.get("email"), new NotNullValidator("邮箱"))
+                .doValidate()
+                .result(ResultCollectors.toComplex());
+        if (!validatResult.isSuccess()) {
+            return new BaseResult(BaseResultEnum.ERROR, validatResult.getErrors().get(0).getErrorMsg());
+        }
+
+        Page<HcAccount> tPage =new Page<HcAccount>(query.getPageNo(),query.getPageSize());
+        tPage.setSearchCount(true);
+
+        Page<HcAccount> result = hcAccountService.selectPage(tPage, query.getEntityWrapper());
+
+        /*List<Integer> ids = new ArrayList<>();
+        ids.add(1);
+        ids.add(2);
+        Page<HcAccount> result = hcAccountService.selectPage(tPage, new EntityWrapper<HcAccount>().in("id", ids));*/
 
 
-        return null;
+        return new BaseResult<Page<HcAccount>>(BaseResultEnum.SUCCESS, result);
     }
 
 }
